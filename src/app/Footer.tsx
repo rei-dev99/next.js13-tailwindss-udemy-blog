b8f57998ca973a6a8eae4d940721739affed9f50
rei <idareiya123@gmail.com>
2024-08-22 21:47:17 +0900

    footer

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='py-2 px-4 border-t'>
+        <small>@2024 reicode</small>
+    </footer>
+  );
+};
+
+export default Footer;
