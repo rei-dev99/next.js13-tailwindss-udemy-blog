864c490c13b8f0c5d9dc25ea4530df0d9af64fbb
rei <idareiya123@gmail.com>
2024-08-22 21:34:44 +0900

    setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
+        <header>header</header>
         {children}
-        </body>
+        <footer>footer</footer>
+      </body>
     </html>
   );
 }
