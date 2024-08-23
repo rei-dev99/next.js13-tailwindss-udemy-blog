0f747ef30d89e00ca50cb7ac0008c1e1065e1ea4
rei <idareiya123@gmail.com>
2024-08-23 10:28:53 +0900

    jsonデータ取得

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,56 +1,49 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Article } from "@/types";
 
-function ArticleList() {
+type ArticleListProps = {
+  articles: Article[];
+};
+
+function ArticleList({ articles }: ArticleListProps) {
   return (
     <div>
-      <article className='shadow my-4 flex flex-col'>
-        <Link href="#" className='hover:opacity-75'>
-          <Image
-            src="https://picsum.photos/800"
-            alt=""
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className='bg-white flex flex-col justify-start p-6'>
-          <Link href="#" className='text-blue-700 pb-4 font-bold'>Technology</Link>
-          <Link href="#" className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'>Next.js勉強中</Link>
-          <p className='text-sm pb-3 text-slate-900'>Published on 2024/08/22</p>
-          <Link href="#" className='text-slate-900 pb-6'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
-            distinctio autem ad illo, adipisci, doloremque tenetur dignissimos,
-            facere veritatis modi odio sapiente nobis reprehenderit iste error!
-            Voluptate id temporibus ab!
+      {articles.map((article) => (
+        <article className="shadow my-4 flex flex-col" key={article.id}>
+          <Link href={`articles/${article.id}`} className="hover:opacity-75">
+            <Image
+              src="https://picsum.photos/800"
+              alt=""
+              width={1280}
+              height={300}
+            />
           </Link>
-          <Link href="#" className='text-pink-800 hover:text-black'>続きを読む</Link>
-        </div>
-      </article>
-      <article className='shadow my-4 flex flex-col'>
-        <Link href="#" className='hover:opacity-75'>
-          <Image
-            src="https://picsum.photos/800"
-            alt=""
-            width={1280}
-            height={300}
-          />
-        </Link>
-        <div className='bg-white flex flex-col justify-start p-6'>
-          <Link href="#" className='text-blue-700 pb-4 font-bold'>Technology</Link>
-          <Link href="#" className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'>Next.js勉強中</Link>
-          <p className='text-sm pb-3 text-slate-900'>Published on 2024/08/22</p>
-          <Link href="#" className='text-slate-900 pb-6'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
-            distinctio autem ad illo, adipisci, doloremque tenetur dignissimos,
-            facere veritatis modi odio sapiente nobis reprehenderit iste error!
-            Voluptate id temporibus ab!
-          </Link>
-          <Link href="#" className='text-pink-800 hover:text-black'>続きを読む</Link>
-        </div>
-      </article>
+          <div className="bg-white flex flex-col justify-start p-6">
+            <Link href="#" className="text-blue-700 pb-4 font-bold">
+              Technology
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+            >
+              {article.title}
+            </Link>
+            <p className="text-sm pb-3 text-slate-900">
+              Published on {article.createdAt}
+            </p>
+            <Link href={`articles/${article.id}`} className="text-slate-900 pb-6">
+              {article.content}
+            </Link>
+            <Link href={`articles/${article.id}`} className="text-pink-800 hover:text-black">
+              続きを読む
+            </Link>
+          </div>
+        </article>
+      ))}
     </div>
   );
-};
+}
 
 export default ArticleList;
