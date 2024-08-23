9b0d7ae612fb532efa72aa53c90cd702ef264db8
rei <idareiya123@gmail.com>
2024-08-23 11:01:31 +0900

    ISRブログ詳細取得用API作成

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,7 +1,11 @@
+import { getDetailArticles } from '@/blogAPI';
 import Image from 'next/image';
 import React from 'react'
 
-const Article = ({ params }: { params: { id: string } }) => {
+const Article = async ({ params }: { params: { id: string } }) => {
+  const detailArticle = await getDetailArticles(params.id);
+  console.log(detailArticle);
+
   return (
     <div className='max-w-3xl mx-auto p-5'>
         <Image
