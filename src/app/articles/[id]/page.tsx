2b66381107ceef30ed90112c37904dcb05ec29f1
rei <idareiya123@gmail.com>
2024-08-23 11:11:38 +0900

    記事内容表示

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 
 const Article = async ({ params }: { params: { id: string } }) => {
   const detailArticle = await getDetailArticles(params.id);
-  console.log(detailArticle);
 
   return (
     <div className='max-w-3xl mx-auto p-5'>
@@ -14,9 +13,9 @@ const Article = async ({ params }: { params: { id: string } }) => {
             width={1280}
             height={300}
         />
-        <h1 className='text-4xl text-center mb-10 mt-10'>ここがタイトルです。</h1>
+        <h1 className='text-4xl text-center mb-10 mt-10'>{detailArticle.title}</h1>
         <div className='text-lg leading-relaxed text-justify'>
-            <p>ここが本文です。</p>
+            <p>{detailArticle.content}</p>
         </div>
     </div>
   );
